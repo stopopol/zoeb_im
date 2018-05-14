0eab3d546f4489e2bfd34f58544504f54fad2236
stopopol <christoph.wohner@umweltbundesamt.at>
2018-05-14 15:26:02 +0200

    Update db_checkRO.php

--- php/db_checkRO.php
+++ php/db_checkRO.php
@@ -23,11 +23,20 @@ $count_no_dupes = count($query_results);
 $duplicates_count =  $count_no_dupes - count($result_no_dupes);
 $duplicates_message = "Number of duplicates: " . $duplicates_count;
 
+
 $report_summary = [];
 
 array_push($report_summary, $RO_number);
 array_push($report_summary, $duplicates_message);
+
+if ($duplicates_count > 0) {
+	$dupes_arr = array_unique(array_diff_assoc($query_results, array_unique($query_results)));
+	array_push($report_summary, $dupes_arr);
+}
+
+
 echo json_encode($report_summary);
+
 
 
 pg_close($dbconn); 
