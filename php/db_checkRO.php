72878584fd26a3758d3a32dbce2a613cb57228b4
stopopol <christoph.wohner@umweltbundesamt.at>
2018-05-15 16:02:42 +0200

    Update db_checkRO.php

--- php/db_checkRO.php
+++ php/db_checkRO.php
@@ -23,7 +23,6 @@ $count_no_dupes = count($query_results);
 $duplicates_count =  $count_no_dupes - count($result_no_dupes);
 $duplicates_message = "Number of duplicates: " . $duplicates_count;
 
-
 $report_summary = [];
 
 array_push($report_summary, $RO_number);
@@ -35,36 +34,42 @@ if ($duplicates_count > 0) {
 	array_push($report_summary, $dupes_arr);
 }
 
-
-// !!! Maybe I should change the order an iterate through the layers and check for the RO in the main table
-
 // search for ROs in layers
-// get list of all layers
-$layers_results = pg_query($conn, "SELECT table_name FROM information_schema.tables WHERE table_schema='public' ORDER BY table_schema,table_name;");
+// get list of all layers containing ROs
+$layers_results = pg_query($conn, "select table_name from INFORMATION_SCHEMA.COLUMNS where COLUMN_NAME like '%Akronym%' order by TABLE_NAME");
 $layer_names = [];
 while ($row = pg_fetch_row($layers_results)) {
   array_push($layer_names,$row[0]);
 }
 
-$test = [];
-// loop through ROs
-$i = 1;
-foreach ($query_results as $raumobjekt) {
-	foreach ($layer_names as $layer ) {
-		if ($layer == "aaRaObj_Master" || $layer == "tst" || $layer == "us_gaz" || $layer == "us_lex" || $layer == "us_rules" || $layer == "spatial_ref_sys" || $layer == "stationcodes_nfnbtrees_combined" || $layer == "stationcodes_nfnbtrees" || $layer == "stationcodes_boundingboxes_polygon" || $layer == "stationcodes_boundingboxes" || $layer == "raster_overviews" || $layer == "raster_columns") {
-			continue;
-		}
-		
-		if ($layer == "im_baeche") {
-			$query_string = 'SELECT "Akronym" FROM "' . $layer . '"' . " WHERE " . '"Akronym" = ' . "'" . $raumobjekt . "'";
-			array_push($test, $query_string);
-			$RO_is_in_layer = pg_query($conn, $query_string);
-		}		
-		
-	}
-} 
 
-//echo json_encode($test);
+// loop through ROs
+
+$query_string = "";
+
+foreach ($layer_names as $layer ) {
+	$query_string = $query_string . 'SELECT "Akronym" FROM "' . $layer . '" UNION ';
+}
+
+$query_string = substr($query_string, 0, -7);
+$query_string = $query_string . ";";
+
+$ROs_results = pg_query($conn, $query_string);
+
+$list_ros_layers = [];
+
+while ($row = pg_fetch_row($ROs_results)) {
+  array_push($list_ros_layers,$row[0]);
+}
+
+// iterate through all ROs
+foreach ($query_results as $ro ) {
+	if (!in_array($ro, $list_ros_layers)) {
+		$RO_redundant_message = "Following RO is not in any layer: " . $ro;
+		array_push($report_summary, $RO_redundant_message);
+	}
+}
+
 echo json_encode($report_summary);
 
 pg_close($dbconn); 
