7775fdd7bfd8c4f93087845e27469c3b03e51aba
stopopol <christoph.wohner@umweltbundesamt.at>
2018-05-16 16:38:01 +0200

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -49,6 +49,27 @@ function check_ro_status() {
 			var number_ros_not_in_main_table = Object.keys(ros_not_in_main_table).length;
 			var total_issues = number_of_duplicates + number_ros_not_in_layers  + number_ros_not_in_main_table;
 			
+			if (number_of_duplicates == 0) {
+				var status_duplicate = '<p style="color:green; text-align: center;">✓</p>';
+			} 
+			else {
+				var status_duplicate = '<p style="color:red; text-align: center;">✕</p>';
+			}
+			if (number_ros_not_in_layers == 0) {
+				var status_ros_not_in_layers = '<p style="color:green; text-align: center;">✓</p>';
+			}
+			else {
+				var status_ros_not_in_layers = '<p style="color:red; text-align: center;">✕</p>';
+			}
+			
+			if (number_ros_not_in_main_table == 0) {
+				var status_ros_not_in_main_table = '<p style="color:green; text-align: center;">✓</p>';
+			}
+			else {
+				var status_ros_not_in_main_table = '<p style="color:red; text-align: center;">✕</p>';
+			}
+			
+			
 			document.getElementById("content_container").innerHTML = "<h3>Status Report - Raumobjekte</h3><small class='text-muted'>This report is automatically generated based on all tables containing an 'Akronym' column</small>";
 			if (total_issues > 0) {
 				if (total_issues == 1) {
@@ -61,7 +82,7 @@ function check_ro_status() {
 			else {
 				document.getElementById("content_container").innerHTML += "<p style='color:green;'>Looks good. No issues were found :)</p>";
 			}
-			document.getElementById("content_container").innerHTML += '<table class="table"> <thead><tr> <th scope="col"></th> <th scope="col">Issue count</th> <th scope="col">"Akronym" of each RO</th></tr> </thead> <tbody><tr> <th scope="row">Duplicates in the main table</th> <td>' + number_of_duplicates + '</td> <td>' + JSON.stringify(list_of_duplicates, null, 2) + '</td></tr><tr> <th scope="row">List of ROs that are not in any layer, but in the main table</th> <td>'+ number_ros_not_in_layers +'</td> <td>'+ JSON.stringify(ros_not_in_layers, null, 2) + '</td></tr><tr> <th scope="row">List of ROs that are not in the main table, but were found in a layer</th> <td>'+ number_ros_not_in_main_table + '</td> <td>'+ JSON.stringify(ros_not_in_main_table, null, 2) +'</td></tr> </tbody></table>';
+			document.getElementById("content_container").innerHTML += '<table class="table"> <thead><tr> <th scope="col"></th> <th scope="col">Issue count</th> <th scope="col">"Akronym" of each RO</th> <th scope="col">Status</th></tr> </thead> <tbody><tr> <th scope="row">Duplicates in the main table</th> <td>' + number_of_duplicates + '</td> <td>' + JSON.stringify(list_of_duplicates, null, 2) + '</td> <td>' + status_duplicate + '</td></tr><tr> <th scope="row">List of ROs that are not in any layer, but in the main table</th> <td>'+ number_ros_not_in_layers +'</td> <td>'+ JSON.stringify(ros_not_in_layers, null, 2) + '</td> <td>'+ status_ros_not_in_layers + '</td></tr><tr> <th scope="row">List of ROs that are not in the main table, but were found in a layer</th> <td>'+ number_ros_not_in_main_table + '</td> <td>'+ JSON.stringify(ros_not_in_main_table, null, 2) + '</td> <td>'+ status_ros_not_in_main_table + '</td></tr> </tbody></table>';
 				/*
 				document.getElementById("content_container").innerHTML += '
 				<table class="table">
@@ -70,6 +91,7 @@ function check_ro_status() {
 					  <th scope="col"></th>
 					  <th scope="col">Issue count</th>
 					  <th scope="col">"Akronym" of each RO</th>
+					  <th scope="col">Status</th>
 					</tr>
 				  </thead>
 				  <tbody>
@@ -77,16 +99,19 @@ function check_ro_status() {
 					  <th scope="row">Duplicates in the main table</th>
 					  <td>' + number_of_duplicates + '</td>
 					  <td>' + JSON.stringify(list_of_duplicates, null, 2) + '</td>
+					  <td>' + status_duplicate + '</td>
 					</tr>
 					<tr>
 					  <th scope="row">List of ROs that are not in any layer, but in the main table</th>
 					  <td>'+ number_ros_not_in_layers +'</td>
 					  <td>'+ JSON.stringify(ros_not_in_layers, null, 2) + '</td>
+					  <td>'+ status_ros_not_in_layers + '</td>
 					</tr>
 					<tr>
 					  <th scope="row">List of ROs that are not in the main table, but were found in a layer</th>
 					  <td>'+ number_ros_not_in_main_table + '</td>
-					  <td>'+ JSON.stringify(ros_not_in_main_table, null, 2) +'</td>
+					  <td>'+ JSON.stringify(ros_not_in_main_table, null, 2) + '</td>
+					  <td>'+ status_ros_not_in_main_table + '</td>
 					</tr>
 				  </tbody>
 				</table>
